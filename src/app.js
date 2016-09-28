451cb5dc0a233c9ce7af5d585c8b9da3fe809a0a
boazblake <boazblake@gmail.com>
2016-09-27 20:36:15 -0500

    home page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'JOSH WEBSITE';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'home'], name: 'home',      
+      moduleId: './home',      nav: true, title: 'HomePage' },
     ]);
 
     this.router = router;
