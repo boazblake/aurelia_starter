1dbe6a658b1fb7d9ef8a62fd690a1eb86bd2a71d
boazblake <boazblake@gmail.com>
2016-09-28 22:20:40 -0500

    added more pages, clean up contact page

--- src/assets/js/app/app.js
+++ src/assets/js/app/app.js
@@ -1,14 +1,14 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'JOSH WEBSITE';
-    config.map([
-      { route: ['', 'home'], name: 'home',      
-      moduleId: '../../../pages/home/home',      nav: false, title: 'HomePage' },
-      { route: ['about'], name: 'about',      
-      moduleId: '../../../pages/about/about',      nav: true, title: 'about us' },
-      { route: ['service'], name: 'service',      
-      moduleId: '../../../pages/service/service',      nav: true, title: 'Our Service' },
-    ]);
+    config.title = 'Aurelia Skeleton';
+    config.map(
+      [
+        { route: ['', 'home'], name: 'home', moduleId: '../../../pages/home/home', nav: false, title: 'HomePage' }
+      , { route: ['about'], name: 'about', moduleId: '../../../pages/about/about', nav: true, title: 'about us' }
+      , { route: ['service'], name: 'service',moduleId: '../../../pages/service/service',nav: true, title: 'Our Service' }
+      , { route: ['information'], name: 'information',moduleId: '../../../pages/information/information',nav: true, title: 'information' }
+      ]
+    );
 
     this.router = router;
   }
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -6,6 +6,7 @@ export class About {
   constructor(imgSrc){
   imgSrc = imgSrc;
   }
+  
   title = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in sem cras amet.';
   subTitle = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum metus et ligula venenatis, at rhoncus nisi molestie. Pellentesque porttitor elit suscipit massa laoreet metus.';
 }
